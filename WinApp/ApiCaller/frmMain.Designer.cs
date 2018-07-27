5be72f0e93c6ce365c3360006ece7aab4e443b80
MADUKA-NB\maduka <maduka_bor@hotmail.com>
2018-07-27 15:10:37 +0800

    加入ApiCaller

---
+++ WinApp/ApiCaller/frmMain.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ApiCaller
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxMethod = new System.Windows.Forms.ComboBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tbResponse = new System.Windows.Forms.TabControl();
+            this.tbResult = new System.Windows.Forms.TabPage();
+            this.tbSource = new System.Windows.Forms.TabPage();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.tbResponse.SuspendLayout();
+            this.tbResult.SuspendLayout();
+            this.tbSource.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxMethod
+            // 
+            this.cbxMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxMethod.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxMethod.FormattingEnabled = true;
+            this.cbxMethod.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"});
+            this.cbxMethod.Location = new System.Drawing.Point(10, 11);
+            this.cbxMethod.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.cbxMethod.Name = "cbxMethod";
+            this.cbxMethod.Size = new System.Drawing.Size(58, 23);
+            this.cbxMethod.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(692, 11);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(53, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(70, 11);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(620, 23);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 53);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(733, 161);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(3, 4);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(719, 216);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(10, 36);
+            this.lblInput.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(42, 15);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "Input";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(10, 216);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(49, 15);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "Output";
+            // 
+            // tbResponse
+            // 
+            this.tbResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResponse.Controls.Add(this.tbResult);
+            this.tbResponse.Controls.Add(this.tbSource);
+            this.tbResponse.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbResponse.Location = new System.Drawing.Point(13, 233);
+            this.tbResponse.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tbResponse.Name = "tbResponse";
+            this.tbResponse.SelectedIndex = 0;
+            this.tbResponse.Size = new System.Drawing.Size(732, 254);
+            this.tbResponse.TabIndex = 7;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Controls.Add(this.txtOutput);
+            this.tbResult.Location = new System.Drawing.Point(4, 26);
+            this.tbResult.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tbResult.Size = new System.Drawing.Size(724, 224);
+            this.tbResult.TabIndex = 0;
+            this.tbResult.Text = "結果";
+            this.tbResult.UseVisualStyleBackColor = true;
+            // 
+            // tbSource
+            // 
+            this.tbSource.Controls.Add(this.txtSource);
+            this.tbSource.Location = new System.Drawing.Point(4, 26);
+            this.tbSource.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tbSource.Size = new System.Drawing.Size(724, 224);
+            this.tbSource.TabIndex = 1;
+            this.tbSource.Text = "原始內容";
+            this.tbSource.UseVisualStyleBackColor = true;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.Location = new System.Drawing.Point(2, 4);
+            this.txtSource.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(720, 216);
+            this.txtSource.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 498);
+            this.Controls.Add(this.tbResponse);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.cbxMethod);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Name = "frmMain";
+            this.Text = "ApiCaller";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tbResponse.ResumeLayout(false);
+            this.tbResult.ResumeLayout(false);
+            this.tbResult.PerformLayout();
+            this.tbSource.ResumeLayout(false);
+            this.tbSource.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxMethod;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TabControl tbResponse;
+        private System.Windows.Forms.TabPage tbResult;
+        private System.Windows.Forms.TabPage tbSource;
+        private System.Windows.Forms.TextBox txtSource;
+    }
+}
+
---
+++ WinApp/ApiCaller/frmMain.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiCaller
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 放上文字的動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 呼叫API的動作
+        /// </summary>
+        /// <param name="strUrl"></param>
+        /// <param name="strHttpMethod"></param>
+        /// <param name="strPostContent"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        protected string CallAPI(string strUrl, string strHttpMethod, string strPostContent, out HttpStatusCode code)
+        {
+            HttpWebRequest request = HttpWebRequest.Create(strUrl) as HttpWebRequest;
+            request.Method = strHttpMethod;
+            code = HttpStatusCode.OK;
+
+            if (strPostContent != "" && strPostContent != string.Empty && strHttpMethod.ToLower() != "get")
+            {
+                request.KeepAlive = true;
+                request.ContentType = "application/json";
+
+                byte[] bs = Encoding.UTF8.GetBytes(strPostContent);
+                Stream reqStream = request.GetRequestStream();
+                reqStream.Write(bs, 0, bs.Length);
+            }
+
+            string strReturn = "";
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    strReturn = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                strReturn = e.Message;
+                code = HttpStatusCode.NotFound;
+            }
+
+            return strReturn;
+        }
+
+        /// <summary>
+        /// 呼叫API的動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                var obj = JsonConvert.DeserializeObject(txtInput.Text);
+                txtInput.Text = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            }
+
+            HttpStatusCode code = HttpStatusCode.OK;
+            string strResponse = this.CallAPI(txtUrl.Text, cbxMethod.Text, txtInput.Text, out code);
+            txtSource.Text = strResponse;
+
+            try
+            {
+                var output = JsonConvert.DeserializeObject(strResponse);
+                txtOutput.Text = JsonConvert.SerializeObject(output, Formatting.Indented);
+            }
+            catch (Exception ex)
+            {
+                txtOutput.Text = ex.Message;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cbxMethod.SelectedIndex = 0;
+        }
+    }
+}
